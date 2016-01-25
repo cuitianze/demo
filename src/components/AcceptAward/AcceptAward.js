7fcdbe65c87376f617bbac65996ce219cb92a61a
root <root@localhost.localdomain>
2016-01-25 14:54:13 +0800

    抽奖页面

--- src/components/AcceptAward/AcceptAward.js
+++ src/components/AcceptAward/AcceptAward.js
@@ -11,7 +11,7 @@ s.body = {
   textAlign: 'center'
 }
 s.title = {
-  marginTop: '100px',
+  marginTop: '10%',
   marginBottom: 0,
   fontSize: '14px',
   fontWeight: 'normal'
@@ -19,9 +19,9 @@ s.title = {
 s.input = {
   width: '62.5%',
   height: '38px',
-  marginTop: '35px',
-  padding: '2px',
-  border: '0',
+  marginTop: '20px',
+  padding: '2px 5px',
+  border: '1px solid #ccc',
   borderRadius: '5px'
 }
 s.btn = {
@@ -33,28 +33,40 @@ s.btn = {
   border: '0',
   borderRadius: '3px'
 }
-s.btnActive = {
+s.resetBtn = {
   width: '31.5%',
   height: '35px',
-  marginTop: '25px',
+  marginTop: '10px',
   color: '#fff',
-  backgroundColor: '#75c202',
+  backgroundColor: 'red',
   border: '0',
-  borderRadius: '3px',
-  color: '62a300'
+  borderRadius: '3px'
+}
+s.resultModal = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0, 0, 0, 0.25)'
+}
+s.resultWrap = {
+  width: '62.5%',
+  margin: '40% auto',
+  backgroundColor: '#fff',
+  borderRadius: '5px'
 }
 
 class AcceptAward extends Component {
   constructor(props) {
     super(props);
     this.getValidCode = this.getValidCode.bind(this);
-    this.acceptAward = this.acceptAward.bind(this);
-    this.toggleClick = this.toggleClick.bind(this);
+    this.getAward = this.getAward.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
   state = {
     validCode: '',
-    click: false,
     awardResult: ''
   }
 
@@ -70,19 +82,21 @@ class AcceptAward extends Component {
     }
   }
 
-  async acceptAward(e) {
+  async getAward(e) {
     e.preventDefault();
     const realCode = this.state.validCode.replace(/\-/g, '');
-    const response = await fetch("http://192.168.1.106:2692/award_activity.wn?param={%22id%22:1}");
+    const response = await fetch("http://192.168.1.106:2692/get_award.wn?param=" + JSON.stringify({code: realCode}));
     const resData = await response.json();
-    this.setState({
-      awardResult: resData.body
-    });
+    if(resData) {
+      this.setState({
+        awardResult: resData.message
+      });
+    }
   }
 
-  toggleClick() {
+  closeModal() {
     this.setState({
-      click: !this.state.click
+      awardResult: ''
     });
   }
 
@@ -90,18 +104,19 @@ class AcceptAward extends Component {
     return (
       <div style={s.body}>
         <h1 style={s.title}>请输入兑奖码</h1>
-        <form onSubmit={this.acceptAward}>
-          <input value={this.state.validCode} type="tel" placeholder="请输入兑奖码" style={s.input} onChange={this.getValidCode} minLength="14" maxLength="14"/>
+        <form onSubmit={this.getAward}>
+          <input value={this.state.validCode} type="tel" placeholder="请输入兑奖码" style={s.input} onChange={this.getValidCode} minLength="14" maxLength="14" required/>
           <br/>
-          <button type="submit" style={this.state.click ? s.btnActive : s.btn}
-            onTouchStart={this.toggleClick}
-            onTouchEnd={this.toggleClick}
-          >提交</button>
+          <button type="submit" style={s.btn}>提交</button>
+          <br/>
+          <button type="reset" style={s.resetBtn}>重置</button>
         </form>
-        <div >
-          <p>{this.state.awardResult}</p>
-          <hr/>
-          <span>确定</span>
+        <div style={!this.state.awardResult ? {display: 'none'} : s.resultModal}>
+          <div style={s.resultWrap}>
+            <p style={{padding: '10px'}}>{this.state.awardResult}</p>
+            <hr/>
+            <span style={{display: 'inline-block', width: '100%', padding: '10px'}} onClick={this.closeModal}>确定</span>
+          </div>
         </div>
       </div>
     )
